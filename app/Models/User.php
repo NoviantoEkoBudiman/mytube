52c7b59ecf615e3d0596f1cb9d21e8b81a3d1a61
Novianto Eko Budiman <stardustvian@gmail.com>
2022-06-14 23:46:20 +0700

    Penambahan fungsi login dan register

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'user_name',
         'email',
         'password',
+        'user_type',
     ];
+
+    protected $primaryKey = "user_id";
 
     /**
      * The attributes that should be hidden for serialization.
